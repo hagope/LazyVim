30f14a47aded5a374ea69aa899fe992687132e6a
Omar Al-Jadda <hagope@gmail.com>
2025-07-28 17:17:29 -0400

    whitespace config

--- lua/plugins/better-whitespace.lua
+++ lua/plugins/better-whitespace.lua
@@ -1,3 +1,18 @@
 return {
   "ntpeters/vim-better-whitespace",
+  config = function()
+    vim.g.better_whitespace_ctermcolor = "gray"
+    vim.g.better_whitespace_guicolor = "gray"
+    vim.g.better_whitespace_filetypes_blacklist = {
+      "nofile",
+      "markdown",
+      "help",
+      "startify",
+      "dashboard",
+      "packer",
+      "neogitstatus",
+      "NvimTree",
+      "Trouble",
+    }
+  end,
 }
