7b411a807b411ca9aededc7b3853912d7d513935
Omar Al-Jadda <omar.aljadda@gmail.com>
2026-03-09 08:29:15 -0700

    whitespace verylazy

--- lua/plugins/better-whitespace.lua
+++ lua/plugins/better-whitespace.lua
@@ -1,8 +1,9 @@
 return {
   "ntpeters/vim-better-whitespace",
+  event = "VeryLazy",
   config = function()
-    vim.g.better_whitespace_ctermcolor = "gray"
-    vim.g.better_whitespace_guicolor = "gray"
+    -- vim.g.better_whitespace_ctermcolor = "gray"
+    -- vim.g.better_whitespace_guicolor = "gray"
     vim.g.better_whitespace_filetypes_blacklist = {
       "nofile",
       "markdown",
